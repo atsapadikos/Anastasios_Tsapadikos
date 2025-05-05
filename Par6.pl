e0a71ba4084c63d3f0db4125b155f58c09a81aa1
Anastasios Tsapadikos <107189484+atsapadikos@users.noreply.github.com>
2025-05-05 17:02:49 +0300

    Add files via upload

---
+++ Par6.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @fruits = ("apple", "banana");
+my $fruit = "grape";
+push @fruits,$fruit;
+print "@fruits \n";
---
+++ Par7.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#hashes
+
+my %fruits = ("apple"=>"red","banana"=>"yellow");
